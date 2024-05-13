da85df68e882785edfcacc6e4dc54d082d8a0db0
eduardo <jose.eduardo@inbec.edu.br>
2024-05-13 10:07:16 -0300

    enviando para as salas corretas

--- public/socket-front-documento.js
+++ public/socket-front-documento.js
@@ -3,11 +3,11 @@ import { atualizaTextoEditor } from "./documento.js";
 const socket = io();
 
 export function selecionarDocumento(nome) {
-    socket.emit("selecionar_documento");
+    socket.emit("selecionar_documento", nome);
 }
 
-export function emitirTextoEditor(texto) {
-    socket.emit("texto_editor", texto);
+export function emitirTextoEditor(dados) {
+    socket.emit("texto_editor", dados);
 }
 
 socket.on("texto_editor_clientes", (texto) => {
--- src/socket-back.js
+++ src/socket-back.js
@@ -1,14 +1,33 @@
 import io from "./server.js";
 
+const documentos = [
+    {
+        nome: "JavaScript",
+        texto: "texto de javascript..."
+    },
+    {
+        nome: "Node",
+        texto: "texto de node..."
+    },
+    {
+        nome: "Socket.io",
+        texto: "texto de socket.io..."
+    },
+]
+
 io.on("connection", (socket) => {
     console.log("A client has connected! ID: ", socket.id);
 
-    socket.on("selecionar_documento", (nome) => {
-        console.log(nome);
+    socket.on("selecionar_documento", (nomeDocumento) => {
+        const documento = encontrarDocumento(nomeDocumento);
+        console.log(documento);
+        socket.join(nomeDocumento);
     })
 
-    socket.on("texto_editor", (texto) => {
-        socket.broadcast.emit("texto_editor_clientes", texto);
+    socket.on("texto_editor", ({ texto, nomeDocumento }) => {
+        // socket.broadcast.emit("texto_editor_clientes", texto);
+
+        socket.to(nomeDocumento).emit("texto_editor_clientes", texto)
     })
 
     socket.on("disconnect", (motivo) => {
@@ -16,3 +35,8 @@ io.on("connection", (socket) => {
         Motivo: ${motivo}`);
     });
 })
+
+function encontrarDocumento(nomeDocumento) {
+    const documento = documentos.find(doc => doc.nome === nomeDocumento);
+    return documento;
+}
